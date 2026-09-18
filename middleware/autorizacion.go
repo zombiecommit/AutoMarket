2e1f8d6befcfbc2a772aca6b5842fdb28977a8ae
TasconLD <lauradayanatascon@hotmail.com>
2026-09-17 22:04:28 -0500

    chore: eliminar RequerirRol sin uso en el proyecto

--- middleware/autorizacion.go
+++ middleware/autorizacion.go
@@ -31,27 +31,3 @@ func RequerirPermiso(operacion string) gin.HandlerFunc {
 	}
 }
 
-// BLOQUE: autorizacion por rol
-func RequerirRol(rolesPermitidos ...string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		usuario, autenticado := ObtenerUsuarioDeContexto(c)
-
-		if !autenticado {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": "se requiere autenticación para realizar esta operación",
-			})
-			return
-		}
-
-		for _, rol := range rolesPermitidos {
-			if usuario.Rol == rol {
-				c.Next()
-				return
-			}
-		}
-
-		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
-			"error": "el rol '" + usuario.Rol + "' no está autorizado para acceder a este recurso",
-		})
-	}
-}
